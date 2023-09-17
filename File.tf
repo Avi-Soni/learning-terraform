acd37a60e6d822983dc1704d3cfca35a27d22cbc
Avi-Soni <145127641+Avi-Soni@users.noreply.github.com>
2023-09-17 16:31:20 +0530

    Update File.tf
    added another file

--- File.tf
+++ File.tf
@@ -7,6 +7,9 @@ resource "local_file" "Test-terraform-file" {
 resource "local_file" "Test-terraform-file1" {
     filename = "C:\\Terraform\\avii1.txt"
     content = "This file is created by Terraform Updated"
-
+}
+resource "local_file" "Test-terraform-file2" {
+    filename = "C:\\Terraform\\avii2.txt"
+    content = "This file is created by Terraform Updated"
   
 }
